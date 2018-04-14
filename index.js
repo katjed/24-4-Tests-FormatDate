f0a5612a5c013c9bffcac8412cb32ecf68adb477
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-04-14 17:57:49 +0200

    Add formatDate

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const formatDate = (timeInSeconds) => {
-  	return `${timeInSeconds}s`;
-}
+const formatDate = (time) => {
+
+	if (!time) {
+		return '0s';
+	}
+
+	var hours = Math.floor(time / 3600);
+	var minutes = Math.floor((time % 3600) / 60);
+	var seconds = Math.floor(time % 60);
+
+	var result = '';
+
+	if (hours) {
+		result += hours + 'h ';
+	}
+
+	if (minutes) {
+		result += minutes + 'm ';
+	}
+
+	if (seconds) {
+		result += seconds + 's ';
+	}
+
+	return result.slice(0, -1);
+};
 
 module.exports = formatDate;
-
